15fef45e3d1c125af6570b284086b32ebb471026
reshu68 <112233476+reshu68@users.noreply.github.com>
2026-07-19 16:40:16 +0530

    Create sg.tf

---
+++ Terraform4/sg.tf
@@ -0,0 +1,17 @@
+resource "aws_security_group" "mysg"{
+    name="mysg"
+    description="sg thorugh terraform"
+
+    ingress {
+        from_port=22
+        to_port=22
+        protocol="tcp"
+        cidr_blocks=["0.0.0.0/0"]
+    }
+    egress {
+        from_port=0
+        to_port=0
+        protocol="-1"
+        cidr_blocks=["0.0.0.0/0"]
+    }
+}
