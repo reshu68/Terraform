9609442863e4e01afcd3edb631b830dedd84bc35
reshu68 <112233476+reshu68@users.noreply.github.com>
2026-08-01 20:45:27 +0530

    Create security-group.tf

---
+++ Terraform7/security-group.tf
@@ -0,0 +1,21 @@
+resource "aws_security_group" "mysg1"{
+    name="oursg"
+    ingress{
+        to_port=22
+        from_port=22
+        protocol="tcp"
+        cidr_blocks=["0.0.0.0/0"]
+    }
+    ingress{
+        to_port=80
+        from_port=80
+        protocol="tcp"
+        cidr_blocks=["0.0.0.0/0"]
+    }
+    egress{
+        to_port=0
+        from_port=0
+        protocol="-1"
+        cidr_blocks=["0.0.0.0/0"]
+    }
+}
